07e70be893919732a5530b75e0532b70514d9537
Дмитрий Бурмак <97616159+DmitriyBurmak@users.noreply.github.com>
2025-03-28 13:53:06 +0200

    stylization 1-timer

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,45 @@
 console.log('snackbar');
+// Імпортуємо бібліотеку iziToast
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+// Отримуємо елементи форми
+const form = document.querySelector('.form');
+const inputDelay = form.querySelector('input[name="delay"]');
+const radioFulfilled = form.querySelector('input[value="fulfilled"]');
+const radioRejected = form.querySelector('input[value="rejected"]');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault(); // Запобігаємо перезавантаженню сторінки
+
+  const delay = Number(inputDelay.value); // Отримуємо затримку
+  const state = radioFulfilled.checked ? 'fulfilled' : 'rejected'; // Визначаємо стан
+
+  // Створюємо проміс
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay); // Виконуємо проміс
+      } else {
+        reject(delay); // Відхиляємо проміс
+      }
+    }, delay);
+  });
+
+  // Обробляємо результат промісу
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
